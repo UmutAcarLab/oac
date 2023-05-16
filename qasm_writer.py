59a902f4c8d5c0e8bd845266e5e5b6763547bc4e
Jatin Arora <jatina29@gmail.com>
2023-05-16 15:26:29 +0000

    add code to change basis and relabel qubits

---
+++ qasm_writer.py
@@ -0,0 +1,33 @@
+from qiskit import *
+import sys
+from math import pi
+from itertools import accumulate
+from qiskit.transpiler import Layout
+
+
+filename = sys.argv[1]
+print("file = ", filename)
+circ = QuantumCircuit.from_qasm_file(filename)
+print("parsing done")
+
+def shit_basis(circ, basis_gates = ['x', 'h', 'rz', 'add', 'cx']):
+  return transpile(circ, basis_gates=basis_gates)
+
+def relabel_qubits (circ):
+  def relabel_list (circ):
+    registers = circ.qregs
+    cnt = 0
+    relabel = []
+    for qr in registers:
+      num_qr = qr.size
+      for i in range(num_qr):
+        relabel.append((qr[i], cnt + i))
+      cnt = cnt + num_qr
+    return relabel
+  rl = relabel_list(circ)
+  return transpile (circ, initial_layout=dict(rl))
+
+def dump (circ, f):
+  circ.qasm(f)
+
+dump(relabel_qubits(shit_basis(circ)), "out.qasm")
