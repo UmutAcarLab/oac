2794682f1516e0464922b3e7df4aa39519d94edd
Jatin Arora <jatina29@gmail.com>
2023-06-01 14:24:32 +0000

    plotter updates

--- plotter.py
+++ plotter.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import glob
 
 def parse_data (d):
+  if d == "":
+    return ([], [])
+  d = d.replace('~', '-').replace('Ee', 'E')
   data_points = d.split(";")
   data_points.pop()
   time_values = []
@@ -17,7 +20,7 @@ def parse_data (d):
 
 def read_quartz_log (f):
   with open (f) as file:
-    return file.read().split('\n')[1]
+    return file.read().split('\n')[-3]
 
 def read_local_log (f):
   with open (f) as file:
@@ -28,26 +31,31 @@ def plot_bench (bn, greedy):
     suffix = '.qasm.greedy.log'
   else:
     suffix = '.qasm.search.log'
-  (dq, dlopt) = (read_quartz_log ("logs/quartz/" + bn + suffix), read_local_log ("logs/lopt/" + bn + suffix))
+  bs = bn + suffix
+  (dq, dlopt) = (read_quartz_log("logs/quartz/" + bs), read_local_log ("logs/lopt/" + bs))
   (tq, szq) = parse_data (dq)
-  plt.plot(tq, szq, marker = 's', label='Quartz', markevery = 20, color = 'blue')
-  plt.plot(tq[-1], szq[-1], marker = 's', color = 'blue')
-  plt.axvline(x=tq[-2], color='red', linestyle='--')
+  print(tq, szq)
+  if (len(tq) >= 2):
+    plt.plot(tq, szq, marker = 's', label='Quartz', color = 'blue')
+    plt.plot(tq[-1], szq[-1], marker = 's', color = 'blue')
+    plt.axvline(x=tq[-2], color='red', linestyle='--')
   (tl, szl) = parse_data (dlopt)
-  plt.plot(tl, szl, marker = 'o', label='Local optimizer', color = 'green')
-  plt.axvline(x=tl[-2], color='red', linestyle='--')
+  szl[0] = szq[0]
+  print(tl, szl)
+  if (len(tl) >= 2):
+    plt.plot(tl, szl, marker = 'o', label='Local optimizer', color = 'green')
+    plt.axvline(x=tl[-2], color='red', linestyle='--')
   plt.xlabel('Time')
   plt.ylabel('Circuit Size')
+  plt.legend()
   if greedy:
     plt.title('%s: Circuit Size vs. Time (greedy)'%(bn.capitalize()))
+    plt.savefig("plots/%s.greedy.png"%(bn), dpi=300)
   else:
     plt.title('%s: Circuit Size vs. Time (search)'%(bn.capitalize()))
-  plt.legend()
-  plt.savefig("plots/%s.png"%(bn), dpi=300)
+    plt.savefig("plots/%s.search.png"%(bn), dpi=300)
 
-
-
-plot_bench ("hhl_n10", True)
+plot_bench ("vbe_adder_3", False)
 # Sample data
 
 # example inputs
@@ -80,7 +88,7 @@ def parse_lopt (greedy):
     suffix = '.qasm.search.log'
   files = log_files_and_names("logs/lopt/", suffix)
   def report_from_file (f, bn):
-    d = read_file (f)
+    d = read_local_log (f)
     (times, sizes) = parse_data(d)
     return (bn, sizes[0], sizes[-1], times [-1])
   results = list(map (lambda f: report_from_file(f[0], f[1]), files))
@@ -94,16 +102,18 @@ def parse_quartz (greedy):
   else:
     suffix = '.qasm.search.log'
   def report_from_file (f, bn):
-    d = read_quartz_log(f)
+    d = read_quartz_log (f)
     (times, sizes) = parse_data(d)
+    if len(sizes) < 2:
+      return (bn, None, None, None)
     return (bn, sizes[0], sizes[-1], times [-1])
   files = log_files_and_names("logs/quartz/", suffix)
   results = list(map (lambda f: report_from_file(f[0], f[1]), files))
   results = sorted(results, key=lambda x: x[0])
   csv_printer(results)
 
-parse_quartz(True)
-# parse_lopt(True)
+# parse_quartz(False)
+# parse_lopt(False)
 # Plotting the graph
 
 
